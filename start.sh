e5986c30bb5a44a9844d71628dd1d39b8b0f69c7
Micael Farias <micaelfarias.dev@gmail.com>
2025-02-27 14:20:06 -0300

    starts

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn seu_projeto.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn project.wsgi:application --bind 0.0.0.0:$PORT
